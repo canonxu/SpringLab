e5e2b48d767c617bb06638ff0a9c93e2e7094de7
canonxu <canonxu@sjtu.edu.cn>
2016-12-23 02:28:48 +0800

    first mapping api

---
+++ src/main/java/org/springlab/springmvc/mapping/MappingController.java
@@ -0,0 +1,15 @@
+package org.springlab.springmvc.mapping;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class MappingController {
+
+    @RequestMapping("/mapping/name")
+    @ResponseBody
+    public String name() {
+        return "canonxu";
+    }
+}
